739c3e7d0981a2464dd0bae6e5db6c01b852d7c2
dvgr-personal <gajjardhruv2001@gmail.com>
2024-09-21 14:42:33 +0530

    Inital commit.

---
+++ source/SimuSCADA.Device.Simulation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SimuSCADA.Device.Simulation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
